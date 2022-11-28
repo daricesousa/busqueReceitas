b9776f6b2967db58f4626a4b9ed42d62db8d1235
“Darice <daricesousa@gmail.com>
2022-11-27 22:12:48 -0300

    measurer plural

--- lib/app/core/utils/enum_measurer.dart
+++ lib/app/core/utils/enum_measurer.dart
@@ -10,31 +10,47 @@ enum Measurer {
   caixa,
   lata,
   pacote;
-  String get display{
-    if(index == 0) return "g";
-    if(index == 1) return "kg";
-    if(index == 2) return "ml";
-    if(index == 3) return "L";
-    if(index == 4) return "unidade";
-    if(index == 5) return "colher";
-    if(index == 6) return "xícara";
-    if(index == 7) return "copo";
-    if(index == 8) return "caixa";
-    if(index == 9) return "lata";
+
+  String get display {
+    if (index == 0) return "g";
+    if (index == 1) return "kg";
+    if (index == 2) return "ml";
+    if (index == 3) return "L";
+    if (index == 4) return "unidade";
+    if (index == 5) return "colher";
+    if (index == 6) return "xícara";
+    if (index == 7) return "copo";
+    if (index == 8) return "caixa";
+    if (index == 9) return "lata";
     return "pacote";
   }
 
-  String get instrution{
-    if(index == 0) return "grama";
-    if(index == 1) return "quilograma";
-    if(index == 2) return "mililitro";
-    if(index == 3) return "litro";
-    if(index == 4) return "unidade";
-    if(index == 5) return "colher de sopa";
-    if(index == 6) return "xícara de chá (240 ml)";
-    if(index == 7) return "copo (200 ml)";
-    if(index == 8) return "caixa";
-    if(index == 9) return "lata";
+  String get instrution {
+    if (index == 0) return "grama";
+    if (index == 1) return "quilograma";
+    if (index == 2) return "mililitro";
+    if (index == 3) return "litro";
+    if (index == 4) return "unidade";
+    if (index == 5) return "colher de sopa";
+    if (index == 6) return "xícara de chá (240 ml)";
+    if (index == 7) return "copo (200 ml)";
+    if (index == 8) return "caixa";
+    if (index == 9) return "lata";
     return "pacote";
   }
+
+  String get displayPlural {
+    if (index == 0) return "g";
+    if (index == 1) return "kg";
+    if (index == 2) return "ml";
+    if (index == 3) return "L";
+    if (index == 4) return "unidades";
+    if (index == 5) return "colheres";
+    if (index == 6) return "xícaras";
+    if (index == 7) return "copos";
+    if (index == 8) return "caixas";
+    if (index == 9) return "latas";
+    return "pacotes";
+  }
 }
+
--- lib/app/models/recipe/recipe_ingredient_model.dart
+++ lib/app/models/recipe/recipe_ingredient_model.dart
@@ -1,3 +1,5 @@
+import 'package:busque_receitas/app/core/utils/enum_measurer.dart';
+
 class RecipeIngredientModel {
   int ingredientId;
   double quantity;
@@ -20,11 +22,28 @@ class RecipeIngredientModel {
     return RecipeIngredientModel(
       ingredientId: json['id'],
       quantity: json['quantity'].toDouble(),
-      measurer: json['measurer'],
+      measurer: _personalizeMeasurer(json['measurer'], json['quantity'].toDouble(),),
     );
   }
+
   @override
   String toString() {
     return ingredientId.toString();
   }
+
+  static String _personalizeMeasurer(String measurerRecipe, double quantity){
+    Measurer? measurer;
+    final listMeasurer = Measurer.values.map((e) => e).toList();
+    for (Measurer m in listMeasurer) {
+      if (measurerRecipe == m.display) {
+        measurer = m;
+      }
+    }
+    if(measurer!= null && quantity >  1 ){
+      return measurer.displayPlural;
+    }
+    return measurerRecipe;
+  }
+
 }
+
