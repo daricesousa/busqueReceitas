acb006eb8ba25bcee7fc09614dfe1466fd58c822
Weliton Sousa <welitonubuntu@gmail.com>
2023-02-16 19:50:46 -0300

    router application json

--- lib/app/core/utils/connect_binding.dart
+++ lib/app/core/utils/connect_binding.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 import 'package:busque_receitas/app/modules/splash/splash_controller.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
@@ -7,20 +8,20 @@ import 'package:get_storage/get_storage.dart';
 class ConnectBinding implements Bindings {
   @override
   void dependencies() {
-    const baseUrl = "http://ec2-3-209-11-224.compute-1.amazonaws.com/";
+    const baseUrl =
+        "https://b6b6-2804-5fb8-c02d-2a00-518a-3b94-601c-d38.sa.ngrok.io";
     final user = GetStorage().read('user') as Map?;
     Map<String, dynamic> headers = {};
-    if (user != null) {
-      headers = {"Authorization": "Bearer ${user['token']}"};
-    }
+    if (user != null) headers["Authorization"] = "Bearer ${user['token']}";
+
     Dio dio = Dio(BaseOptions(
       receiveTimeout: const Duration(seconds: 60),
       connectTimeout: const Duration(seconds: 60),
       sendTimeout: const Duration(seconds: 60),
       baseUrl: baseUrl,
-      headers: headers,
+      headers: {...headers, HttpHeaders.contentTypeHeader: "application/json"},
     ));
-    
+
     Get.put(dio);
     Get.put(SplashController());
   }
