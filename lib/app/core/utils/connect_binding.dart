33deed867455c412cf80abb22dca653db0ee3d05
“Darice <daricesousa@gmail.com>
2023-02-16 14:57:57 -0300

    change server api and allow http routes

--- lib/app/core/utils/connect_binding.dart
+++ lib/app/core/utils/connect_binding.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:busque_receitas/app/modules/splash/splash_controller.dart';
-import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -8,22 +7,20 @@ import 'package:get_storage/get_storage.dart';
 class ConnectBinding implements Bindings {
   @override
   void dependencies() {
-    const baseUrl = "https://busque-receitas.onrender.com";
+    const baseUrl = "http://ec2-3-209-11-224.compute-1.amazonaws.com/";
     final user = GetStorage().read('user') as Map?;
     Map<String, dynamic> headers = {};
     if (user != null) {
       headers = {"Authorization": "Bearer ${user['token']}"};
     }
     Dio dio = Dio(BaseOptions(
+      receiveTimeout: const Duration(seconds: 60),
+      connectTimeout: const Duration(seconds: 60),
+      sendTimeout: const Duration(seconds: 60),
       baseUrl: baseUrl,
       headers: headers,
     ));
-    (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
-        (HttpClient client) {
-      client.badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-      return client;
-    };
+    
     Get.put(dio);
     Get.put(SplashController());
   }
